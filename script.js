2f90a3aed183166632a2102eadc15bb4c481e503
CodingXamuyee <127799815+TheXamuyee@users.noreply.github.com>
2023-07-01 20:56:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,34 +14,112 @@ const music = new Audio();
 
 const songs = [
     {
+        path: 'assets/01.mp3',
+        displayName: 'Rauw Alejandro: Bzrp Music',
+        cover: 'assets/01.jpg',
+        artist: 'Bizarrap, Rauw Alejandro',
+    },
+    {
         path: 'assets/1.mp3',
         displayName: 'Neutro Shorty: Bzrp Music',
         cover: 'assets/1.jpg',
-        artist: 'Bizarrap',
+        artist: 'Bizarrap, Neutro Shorty',
     },
     {
         path: 'assets/2.mp3',
         displayName: 'Big Soto: Bzrp Music',
         cover: 'assets/2.jpg',
-        artist: 'Bizarrap',
+        artist: 'Bizarrap, Big Soto',
     },
     {
         path: 'assets/3.mp3',
         displayName: 'Zaramay: Bzrp Music',
         cover: 'assets/3.jpg',
-        artist: 'Bizarrap',
+        artist: 'Bizarrap, ZARAMAY',
     },
     {
         path: 'assets/4.mp3',
-        displayName: 'Big Booty',
+        displayName: 'Dani Ribba: Bzrp Music',
         cover: 'assets/4.jpg',
-        artist: 'Hozwal, Young Miko, Lil Geniuz',
+        artist: 'Bizarrap, Dani Ribba',
     },
     {
         path: 'assets/5.mp3',
-        displayName: 'Lisa',
+        displayName: 'Big Booty',
         cover: 'assets/5.jpg',
+        artist: 'Hozwal, Young Miko, Lil Geniuz',
+    },
+    {
+        path: 'assets/6.mp3',
+        displayName: 'Lisa',
+        cover: 'assets/6.jpg',
         artist: 'Young Miko',
+    },
+    {
+        path: 'assets/7.mp3',
+        displayName: 'My Name',
+        cover: 'assets/7.jpg',
+        artist: 'Neutro Shorty',
+    },
+    {
+        path: 'assets/8.mp3',
+        displayName: 'The World is mine',
+        cover: 'assets/8.jpg',
+        artist: 'Neutro Shorty',
+    },
+    {
+        path: 'assets/9.mp3',
+        displayName: 'Hennessy',
+        cover: 'assets/9.jpg',
+        artist: 'Neutro Shorty, Big Soto',
+    },
+    {
+        path: 'assets/10.mp3',
+        displayName: 'DUBAI',
+        cover: 'assets/10.jpg',
+        artist: 'Trueno, Beny Jr',
+    },
+    {
+        path: 'assets/11.mp3',
+        displayName: 'Lastima',
+        cover: 'assets/11.jpg',
+        artist: 'Neutro Shorty, Selected Music',
+    },
+    {
+        path: 'assets/12.mp3',
+        displayName: 'Tal Vez',
+        cover: 'assets/12.jpg',
+        artist: 'Paulo Londra',
+    },
+    {
+        path: 'assets/13.mp3',
+        displayName: 'Adan y Eva',
+        cover: 'assets/13.jpg',
+        artist: 'Paulo Londra',
+    },
+    {
+        path: 'assets/14.mp3',
+        displayName: 'Homerun',
+        cover: 'assets/14.jpg',
+        artist: 'Paulo Londra',
+    },
+    {
+        path: 'assets/15.mp3',
+        displayName: 'Rara Vez',
+        cover: 'assets/15.jpg',
+        artist: 'Taiu, Milo J',
+    },
+    {
+        path: 'assets/16.mp3',
+        displayName: 'Milagrosa',
+        cover: 'assets/16.jpg',
+        artist: 'Milo J',
+    },
+    {
+        path: 'assets/17.mp3',
+        displayName: 'Disfruto',
+        cover: 'assets/17.jpg',
+        artist: 'Carla Morrison',
     }
 ];
 
